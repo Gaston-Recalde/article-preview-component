cee7b0645523402a53b0b88b4deccaf437d889c6
Gaston Recalde <gastonrg94@gmail.com>
2022-11-22 10:11:35 -0300

    Proyecto terminado

--- app.js
+++ app.js
@@ -10,11 +10,5 @@ const init = () => {
     document.getElementById("buttonClose").style.display = 'none';
 }
 
-// function init(){
-//     document.querySelector(".share").style.display = 'none';
-//     document.getElementById("button").style.display = 'inline-block';
-//     document.getElementById("buttonClose").style.display = 'none';
-// };
-
 document.getElementById("buttonClose").addEventListener('click', init);
 document.getElementById("buttonCloseOne").addEventListener('click', init);
